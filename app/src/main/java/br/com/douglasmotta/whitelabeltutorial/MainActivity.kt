76df8c823bbcedeff449cff1bb10f273b0bcb457
Douglas Motta <tibeca@gmail.com>
2021-06-26 16:53:16 +0100

    Skeleton

---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.douglasmotta.whitelabeltutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import br.com.douglasmotta.whitelabeltutorial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
