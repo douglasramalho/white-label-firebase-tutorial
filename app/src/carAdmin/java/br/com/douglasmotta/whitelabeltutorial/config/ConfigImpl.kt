92fb9e4534827864effc5a0e106bd89ab144aaf3
Douglas Motta <tibeca@gmail.com>
2021-07-17 14:54:27 +0100

    Final adjustments

--- app/src/carAdmin/java/br/com/douglasmotta/whitelabeltutorial/config/ConfigImpl.kt
+++
@@ -1,8 +0,0 @@
-package br.com.douglasmotta.whitelabeltutorial.config
-
-import android.view.View
-import javax.inject.Inject
-
-class ConfigImpl @Inject constructor() : Config {
-    override val addButtonVisibility = View.VISIBLE
-}
