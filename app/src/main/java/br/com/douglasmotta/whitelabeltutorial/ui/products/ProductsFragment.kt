48cfb06ebfa8c089efc4ee847108334b7a586d2a
Douglas Motta <tibeca@gmail.com>
2021-08-01 15:13:32 +0100

    Vídeo 13

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
@@ -5,12 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentProductsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProductsFragment : Fragment() {
 
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ProductsViewModel by viewModels()
+
+    private val productsAdapter = ProductsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +26,31 @@ class ProductsFragment : Fragment() {
     ): View {
         _binding = FragmentProductsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
+        observeVMEvents()
+
+        viewModel.getProducts()
+    }
+
+    private fun setRecyclerView() {
+        binding.recyclerProducts.run {
+            setHasFixedSize(true)
+            adapter = productsAdapter
+        }
+    }
+
+    private fun observeVMEvents() {
+        viewModel.productsData.observe(viewLifecycleOwner) { products ->
+            productsAdapter.submitList(products)
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsViewModel.kt
@@ -0,0 +1,30 @@
+package br.com.douglasmotta.whitelabeltutorial.ui.products
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
+import br.com.douglasmotta.whitelabeltutorial.domain.usecase.GetProductsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductsViewModel @Inject constructor(
+    private val getProductsUseCase: GetProductsUseCase
+) : ViewModel() {
+
+    private val _productsData = MutableLiveData<List<Product>>()
+    val productsData: LiveData<List<Product>> = _productsData
+
+    fun getProducts() = viewModelScope.launch {
+        try {
+            val products = getProductsUseCase()
+            _productsData.value = products
+        } catch (e: Exception) {
+            Log.d("ProductsViewModel", e.toString())
+        }
+    }
+}
