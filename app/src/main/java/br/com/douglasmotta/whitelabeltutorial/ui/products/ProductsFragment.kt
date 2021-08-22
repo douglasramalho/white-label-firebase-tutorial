2b08a64216c7a1c8733b39e798856850182f4166
Douglas Motta <tibeca@gmail.com>
2021-08-22 23:36:50 +0100

    Video 15

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
@@ -41,7 +41,7 @@ class ProductsFragment : Fragment() {
         observeNavBackStack()
         observeVMEvents()
 
-        viewModel.getProducts()
+        getProducts()
     }
 
     private fun setRecyclerView() {
@@ -52,9 +52,19 @@ class ProductsFragment : Fragment() {
     }
 
     private fun setListeners() {
-        binding.fabAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_productsFragment_to_addProductFragment)
+        with(binding) {
+            swipeProducts.setOnRefreshListener {
+                getProducts()
+            }
+
+            fabAdd.setOnClickListener {
+                findNavController().navigate(R.id.action_productsFragment_to_addProductFragment)
+            }
         }
+    }
+
+    private fun getProducts() {
+        viewModel.getProducts()
     }
 
     private fun observeNavBackStack() {
@@ -86,6 +96,7 @@ class ProductsFragment : Fragment() {
 
     private fun observeVMEvents() {
         viewModel.productsData.observe(viewLifecycleOwner) { products ->
+            binding.swipeProducts.isRefreshing = false
             productsAdapter.submitList(products)
         }
 
